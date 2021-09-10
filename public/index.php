69b138a44bf9d05bdd29cdabd8e268ec6d20af84
Mathias <mat.micheli99@gmail.com>
2021-09-10 11:07:02 +0200

    upgrade: bootstrap 3 to 5

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+define('ROOT_DIR', basename(dirname(__FILE__)));
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
