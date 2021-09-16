6bbf421289d3fd4552e2f142577773ce62ebac71
Mathias <mat.micheli99@gmail.com>
2021-09-16 16:50:09 +0200

    test: unit test on formtype

---
+++ tests/Form/UserTypeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Form;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class UserTypeTest extends TypeTestCase
+{
+    public function testSubmitValidData()
+    {
+        $formData = [
+            'username' => 'Mathias',
+            'email' => 'mail@mail.com',
+            'password' => [
+                'first' => 'password',
+                'second' => 'password',
+            ],
+            'roles' => [
+                'ROLE_USER'
+            ]
+        ];
+
+        $model = new User();
+
+        $form = $this->factory->create(UserType::class, $model);
+
+        $excepted = (new User())->setUsername('Mathias')->setEmail('mail@mail.com')->setPassword('password');
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertTrue($form->isSubmitted());
+        $this->assertEquals($excepted, $model);
+    }
+}
